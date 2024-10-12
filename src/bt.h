e432f121a10cfb038b2a1c64e6528044b67f2eb5
machadoleonardo94 <52208834+machadoleonardo94@users.noreply.github.com>
2024-10-12 18:23:12 -0300

    Add PCB schematic

--- src/bt.h
+++ src/bt.h
@@ -3,40 +3,46 @@
 
 #include "display.h"
 
-void avrc_metadata_callback(uint8_t id, const uint8_t *text) {
+void avrc_metadata_callback(uint8_t id, const uint8_t *text)
+{
   Serial.printf("==> AVRC metadata rsp: attribute id 0x%x, %s\n", id, text);
-  switch (id) {
-    case ESP_AVRC_MD_ATTR_TITLE:
-      title = String((char*)text);
-      break;
-    case ESP_AVRC_MD_ATTR_ARTIST:
-      artist = String((char*)text);
-      break;
-    case ESP_AVRC_MD_ATTR_ALBUM:
-      album = String((char*)text);
-      break;
-    case ESP_AVRC_MD_ATTR_PLAYING_TIME:
-      durTime = String((char*)text).toInt();
-      break;
+  switch (id)
+  {
+  case ESP_AVRC_MD_ATTR_TITLE:
+    title = String((char *)text);
+    break;
+  case ESP_AVRC_MD_ATTR_ARTIST:
+    artist = String((char *)text);
+    break;
+  case ESP_AVRC_MD_ATTR_ALBUM:
+    album = String((char *)text);
+    break;
+  case ESP_AVRC_MD_ATTR_PLAYING_TIME:
+    durTime = String((char *)text).toInt();
+    break;
   }
   updateDisplay();
 }
 
-void read_data_stream(const uint8_t *data, uint32_t length) {
+void read_data_stream(const uint8_t *data, uint32_t length)
+{
   i2s.write(data, length);
 }
 
-void playPositionCallback(uint32_t play_pos) {
+void playPositionCallback(uint32_t play_pos)
+{
   playtime = play_pos;
 }
 
-void playbackStatusCallback(esp_avrc_playback_stat_t Playing) {
+void playbackStatusCallback(esp_avrc_playback_stat_t Playing)
+{
   isPlaying = Playing;
 }
 
-void setupBT() {
+void setupBT()
+{
 
-  a2dp_sink.set_avrc_metadata_attribute_mask(ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_ARTIST | ESP_AVRC_MD_ATTR_ALBUM | ESP_AVRC_MD_ATTR_PLAYING_TIME );
+  a2dp_sink.set_avrc_metadata_attribute_mask(ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_ARTIST | ESP_AVRC_MD_ATTR_ALBUM | ESP_AVRC_MD_ATTR_PLAYING_TIME);
   a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
   a2dp_sink.set_avrc_rn_playstatus_callback(playbackStatusCallback);
   a2dp_sink.set_avrc_rn_play_pos_callback(playPositionCallback, 1);
@@ -48,44 +54,48 @@ void setupBT() {
   // setup output
   auto cfg = i2s.defaultConfig();
   cfg.copyFrom(info);
-  //cfg.signal_type = PDM;
+  cfg.signal_type = PDM;
   cfg.pin_data = 23;
   cfg.buffer_count = 8;
   cfg.buffer_size = 256;
   i2s.begin(cfg);
-
 }
 
-void parseIRCommand(uint32_t irCode) {
-  switch (irCode) {
-    case IR_CMD_PLAY_PAUSE:
-      if (isPlaying) {
-        a2dp_sink.pause();
-      } else {
-        a2dp_sink.play();
-      }
-      isPlaying = !isPlaying;
-      break;
-    case IR_CMD_VOLUME_UP:
-      a2dp_sink.set_volume(volume++);
-      break;
-    case IR_CMD_VOLUME_DOWN:
-      a2dp_sink.set_volume(volume--);
-      break;
-      case IR_CMD_VOLUME_MUTE:
-      a2dp_sink.set_volume(0);
-      break;
-    case IR_CMD_FORWARD:
-      a2dp_sink.next();
-      break;
-    case IR_CMD_BACKWARD:
-      a2dp_sink.previous();
-      break;
-    case IR_CMD_POWER:
-      digitalWrite(RELAY_PIN, !digitalRead(RELAY_PIN));
-      break;
-    default:
-      break;
+void parseIRCommand(uint32_t irCode)
+{
+  switch (irCode)
+  {
+  case IR_CMD_PLAY_PAUSE:
+    if (isPlaying)
+    {
+      a2dp_sink.pause();
+    }
+    else
+    {
+      a2dp_sink.play();
+    }
+    isPlaying = !isPlaying;
+    break;
+  case IR_CMD_VOLUME_UP:
+    a2dp_sink.set_volume(volume++);
+    break;
+  case IR_CMD_VOLUME_DOWN:
+    a2dp_sink.set_volume(volume--);
+    break;
+  case IR_CMD_VOLUME_MUTE:
+    a2dp_sink.set_volume(0);
+    break;
+  case IR_CMD_FORWARD:
+    a2dp_sink.next();
+    break;
+  case IR_CMD_BACKWARD:
+    a2dp_sink.previous();
+    break;
+  case IR_CMD_POWER:
+    digitalWrite(RELAY_PIN, !digitalRead(RELAY_PIN));
+    break;
+  default:
+    break;
   }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include "bt.h"
 #include "ir.h"
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Wire.begin(22, 18);
   pinMode(RELAY_PIN, OUTPUT);
@@ -15,14 +16,13 @@ void setup() {
   IrReceiver.begin(IR_RECEIVER_PIN, DISABLE_LED_FEEDBACK);
   Serial.print(F("Ready to receive IR signals of protocols: "));
   printActiveIRProtocols(&Serial);
-  
-  a2dp_sink.set_auto_reconnect(true, 10)
-  {
-    a2dp_sink.start("NiceAmp"); // Start Bluetooth Audio Receiver
-  }
+
+  a2dp_sink.set_auto_reconnect(true, 10);
+  a2dp_sink.start("NiceAmp"); // Start Bluetooth Audio Receiver
 }
 
-void loop() {
+void loop()
+{
   decodeIR();
   updateDisplay();
 
